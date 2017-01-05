ab1e8043a7e9287f894ef4f505e6f25dd1c31066
hayo <hayo@parduhn.de>
2017-01-05 13:54:32 +0100

    CORS fix

--- build_and_start_docker.sh
+++ build_and_start_docker.sh
@@ -1,3 +1,4 @@
 #! /usr/bash
+docker rmi rest-service -f
 docker build -t rest-service .
 docker run -p 5001:5001 -d rest-service
--- src/index.js
+++ src/index.js
@@ -4,11 +4,31 @@ var server = restify.createServer({
   name: 'myapp',
   version: '1.0.0'
 });
+
+server.use(restify.CORS({
+
+  // Defaults to ['*'].
+  origins: ['https://localhost:80'], 
+  // Defaults to false.
+  // credentials: true,
+  // Sets expose-headers.
+  headers: [    'accept',
+        'accept-version',
+        'content-type',
+        'request-id',
+        'origin',
+        'x-api-version',
+        'x-request-id'
+        ] 
+
+}));
+
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 server.get('/echo/:name', function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin','*');
   res.send(req.params);
   return next();
 });
