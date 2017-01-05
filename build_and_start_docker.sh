968719e6ba3eb4c543b989f9c8b30fae2cd7102e
hayo <hayo@parduhn.de>
2017-01-05 15:53:51 +0100

    docker script

--- build_and_start_docker.sh
+++ build_and_start_docker.sh
@@ -1,4 +1,5 @@
 #! /usr/bash
+docker stop rest-service
 docker rmi rest-service -f
 docker build -t rest-service .
-docker run -p 5001:5001 -d rest-service
+docker run -p 5001:5001 -d --name rest-service rest-service
