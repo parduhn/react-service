dd22c06eb3f4c1c14394bdcfbf22206f6fec4bc8
hayo <hayo@parduhn.de>
2017-01-05 16:06:26 +0100

    git_push.sh

--- build_and_start_docker.sh
+++ build_and_start_docker.sh
@@ -1,5 +1,16 @@
 #! /usr/bash
+
+
+#! /usr/bash
+echo Stop container:
 docker stop rest-service
+echo Remove container
+docker rm rest-service -f
+echo Remove image:
 docker rmi rest-service -f
+echo Build image:
 docker build -t rest-service .
-docker run -p 5001:5001 -d --name rest-service rest-service
+echo Run container:
+docker run -p 80:80 -d --name rest-service rest-service
+
+
